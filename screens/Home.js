d2e6e6a63ee6193bb5a30a3e9910ad9ec6516285
danielkimassai <d.kakai@kokonetworks.com>
2019-10-25 13:41:44 +0300

    friday 25

---
+++ screens/Home.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { Block, theme } from 'galio-framework';
+
+import { Card } from '../components';
+import articles from '../constants/articles';
+const { width } = Dimensions.get('screen');
+
+class Home extends React.Component {
+  renderArticles = () => {
+    return (
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.articles}>
+        <Block flex> 
+          <Block flex row>
+            <Card item={articles[1]} style={{ marginRight: theme.SIZES.BASE }} />
+            <Card item={articles[2]} />
+          </Block>  
+        </Block>
+      </ScrollView>
+    )
+  }
+
+  render() {
+    return (
+      <Block flex center style={styles.home}>
+        {this.renderArticles()}
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  home: {
+    width: width,    
+  },
+  articles: {
+    width: width - theme.SIZES.BASE * 2,
+    paddingVertical: theme.SIZES.BASE,
+  },
+});
+
+export default Home;
---
+++ screens/Library.js
@@ -0,0 +1,210 @@
+import React from "react";
+import { Easing, Animated } from "react-native";
+import {
+  createStackNavigator,
+  createDrawerNavigator,
+  createAppContainer
+} from "react-navigation";
+
+import { Block } from "galio-framework";
+
+// screens
+import Home from "../screens/Home";
+import Pro from "../screens/Pro";
+import Profile from "../screens/Profile";
+import Register from "../screens/Register";
+import Elements from "../screens/Elements";
+import Articles from "../screens/Articles";
+import Categ from "../screens/Categ";
+
+
+// drawer
+import Menu from "../navigation/Menu";
+import DrawerItem from "../components/DrawerItem";
+
+// header for screens
+import Header from "../components/Header";
+
+const transitionConfig = (transitionProps, prevTransitionProps) => ({
+  transitionSpec: {
+    duration: 400,
+    easing: Easing.out(Easing.poly(4)),
+    timing: Animated.timing
+  },
+  screenInterpolator: sceneProps => {
+    const { layout, position, scene } = sceneProps;
+    const thisSceneIndex = scene.index;
+    const width = layout.initWidth;
+
+    const scale = position.interpolate({
+      inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+      outputRange: [4, 1, 1]
+    });
+    const opacity = position.interpolate({
+      inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+      outputRange: [0, 1, 1]
+    });
+    const translateX = position.interpolate({
+      inputRange: [thisSceneIndex - 1, thisSceneIndex],
+      outputRange: [width, 0]
+    });
+
+    const scaleWithOpacity = { opacity };
+    const screenName = "Search";
+
+    if (
+      screenName === transitionProps.scene.route.routeName ||
+      (prevTransitionProps &&
+        screenName === prevTransitionProps.scene.route.routeName)
+    ) {
+      return scaleWithOpacity;
+    }
+    return { transform: [{ translateX }] };
+  }
+});
+
+const ElementsStack = createStackNavigator({
+  Elements: {
+    screen: Elements,
+    navigationOptions: ({ navigation }) => ({
+      header: <Header title="Elements" navigation={navigation} />
+    })
+  }
+},{
+  cardStyle: {
+    backgroundColor: "#F8F9FE"
+  },
+  transitionConfig
+});
+
+const CategStack = createStackNavigator({
+  Categ: {
+    screen: Categ,
+    navigationOptions: ({ navigation }) => ({
+      header: <Header title="Categ" navigation={navigation} />
+    })
+  }
+},{
+  cardStyle: {
+    backgroundColor: "#F8F9FE"
+  },
+  transitionConfig
+});
+
+const ArticlesStack = createStackNavigator({
+  Articles: {
+    screen: Articles,
+    navigationOptions: ({ navigation }) => ({
+      header: <Header title="Articles" navigation={navigation} />
+    })
+  }
+},{
+  cardStyle: {
+    backgroundColor: "#F8F9FE"
+  },
+  transitionConfig
+});
+
+const ProfileStack = createStackNavigator(
+  {
+    Profile: {
+      screen: Profile,
+      navigationOptions: ({ navigation }) => ({
+        header: (
+          <Header white transparent title="Profile" iconColor={'#FFF'} navigation={navigation} />
+        ),
+        headerTransparent: true
+      })
+    }
+  },
+  {
+    cardStyle: { backgroundColor: "#FFFFFF" },
+    transitionConfig
+  }
+);
+
+
+const HomeStack = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: ({ navigation }) => ({
+        header: <Header search options title="Home" navigation={navigation} />
+      })
+    },
+    Pro: {
+      screen: Pro,
+      navigationOptions: ({ navigation }) => ({
+        header: (
+          <Header left={<Block />} white transparent title="" navigation={navigation} />
+        ),
+        headerTransparent: true
+      })
+    }
+  },
+  {
+    cardStyle: {
+      backgroundColor: "#F8F9FE"
+    },
+    transitionConfig
+  }
+);
+// divideru se baga ca si cum ar fi un ecrna dar nu-i nimic duh
+const AppStack = createDrawerNavigator(
+  {
+    
+    Home: {
+      screen: HomeStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} title="Home" />
+        )
+      })
+    },
+    
+    Profile: {
+      screen: ProfileStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Profile" title="Profile" />
+        )
+      })
+    },
+    Account: {
+      screen: Register,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Register" title="Account" />
+        )
+      })
+    },
+    Elements: {
+      screen: ElementsStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Elements" title="Elements" />
+        )
+      })
+    },
+    Categ: {
+      screen: CategStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Categ" title="Categ" />
+        )
+      })
+    },
+    Articles: {
+      screen: ArticlesStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Articles" title="Articles" />
+        )
+      })
+    }
+  },
+  Menu
+);
+
+const AppContainer = createAppContainer(AppStack);
+export default AppContainer;
